25abc8ee6e5c62d69dba7ba275a03843d6e3f593
manhdhdn <huumanh.bmt.dn@gmail.com>
2023-06-19 12:25:51 +0700

    Remove not found page

--- ClientApp/src/AppRoutes.js
+++ ClientApp/src/AppRoutes.js
@@ -1,5 +1,4 @@
 import Home from "pages/Home";
-import NotFound from "./pages/NotFound";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Profile from "pages/Profile";
