35e53757db832490e1703f557a58c3b8ef5566b2
manhdhdn <huumanh.bmt.dn@gmail.com>
2023-06-28 20:04:59 +0700

    Edit on cart function

--- ClientApp/src/components/Navbar/index.jsx
+++ ClientApp/src/components/Navbar/index.jsx
@@ -9,13 +9,22 @@ import OrderApi from "apis/services/Order";
 import OrderDetailApi from "apis/services/OrderDetail";
 
 import { DeleteForever } from "@mui/icons-material";
-import { Button, Img, Line, Text } from "components";
+import { Backdrop, Checkbox } from "@mui/material";
+import { Button, Img, Input, Line, Text } from "components";
 
 const Navbar = (props) => {
   const [userLoggedIn, setUserLoggedIn] = useState(true);
   const [isOpen, setIsOpen] = useState(false);
   const [isOpenCart, setIsOpenCart] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [number, setNumber] = useState({
+    set1: 1,
+    set2: 0,
+    set4: 0,
+    set6: 0
+  });
   const [cart, setCart] = useState(null);
+  const [orderDetail, setOrderDetail] = useState(null);
   const [userIcon, setUserIcon] = useState("images/img_user.svg");
   const [cartIcon, setCartIcon] = useState("images/img_cart.svg");
   const [hoverEdit, setHoverEdit] = useState(-1);
@@ -25,6 +34,8 @@ const Navbar = (props) => {
   const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
   const { logout } = UserAuth();
+
+  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
   useEffect(() => {
     var user = localStorage.getItem("token");
@@ -145,6 +156,199 @@ const Navbar = (props) => {
     navigate(`/payment/${cart.id}`);
   }
 
+  const handleOpenEditPopup = (orderDetail) => {
+    setOpen(true);
+    setIsOpenCart(false);
+    setOrderDetail(orderDetail);
+
+    let cakeSet = JSON.parse(orderDetail.cakeSet);
+
+    setNumber({ ...number, set1: cakeSet.set1 === undefined ? 0 : cakeSet.set1, set2: cakeSet.set2 === undefined ? 0 : cakeSet.set2, set4: cakeSet.set4 === undefined ? 0 : cakeSet.set4, set6: cakeSet.set6 === undefined ? 0 : cakeSet.set6 });
+  }
+
+  const handleCloseEditPopup = () => {
+    setOpen(false);
+  }
+
+  const handleInputNumber = (value, set) => {
+    if (value < 0) {
+      setNumber({ ...number, [set]: 0 });
+      return;
+    }
+
+    if (value > 10) {
+      setNumber({ ...number, [set]: 10 });
+      return;
+    }
+
+    setNumber({ ...number, [set]: value !== '' ? parseInt(value.toString()) : value });
+  }
+
+  const handleInputBlur = (set) => {
+    if (number[set] === '') {
+      setNumber({ ...number, [set]: 0 });
+    }
+  }
+
+  const handleCheckBoxClick = (set) => {
+    if (number[set] === 0) {
+      setNumber({ ...number, [set]: 1 });
+    } else {
+      setNumber({ ...number, [set]: 0 });
+    }
+  }
+
+  const handleUpdateCart = async () => {
+    try {
+      let orderId = cart.id;
+      let cake = orderDetail.cake;
+      let quantity = 0;
+      let cakeSet = null;
+      let status = 0;
+
+      Object.keys(number).forEach((key) => {
+        if (number[key] !== 0) {
+          quantity += number[key] * parseInt(key.slice(3));
+          cakeSet = { ...cakeSet, [key]: number[key] };
+        }
+      })
+
+      if (cart) {
+        status = await OrderDetailApi.updateOrderDetail(orderDetail.id, {
+          id: orderDetail.id,
+          orderId,
+          cakeId: cake.id,
+          price: cake.price,
+          quantity,
+          cakeSet: JSON.stringify(cakeSet)
+        })
+
+        if (status === 204) {
+          enqueueSnackbar("Updated cart", { variant: "success" });
+
+          let cart = await OrderApi.getOrder(orderId);
+          localStorage.setItem("cart", JSON.stringify(cart));
+
+          handleCloseEditPopup();
+        }
+      }
+    } catch (error) {
+      enqueueSnackbar("Update cart failed", { variant: "info" });
+    }
+  }
+
+  const editCake = () => {
+    return (
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+      >
+        <div className="h-[45%] bg-orange-50 border border-red-500 flex flex-col items-center justify-center rounded-[5px] p-12 w-[40%]">
+          <div className="flex flex-col font-sfmono gap-4 items-center justify-start w-full">
+            <div className="flex flex-col gap-3.5 items-center justify-start w-full">
+              <Line className="bg-red-500 h-px w-full" />
+              <div className="flex flex-row items-center justify-between w-full">
+                <div className="flex flex-col items-center justify-start">
+                  <div className="flex flex-row gap-[25px] items-center justify-between w-full">
+                    <Text className="text-lg text-red-500">Set:</Text>
+                    <div className="flex flex-col items-center justify-center">
+                      <div className="flex flex-row items-center justify-start w-full">
+                        <Checkbox
+                          {...label}
+                          checked={number["set1"] !== 0}
+                          color="success"
+                          onChange={() => handleCheckBoxClick("set1")}
+                        />
+                        <Text className="font-sfmono text-red-500 text-left">Set 1 cake</Text>
+                        <Input
+                          className={`transition-all duration-300 leading-[normal] text-center text-lg text-red-500 h-full w-full ${number["set1"] === 0 ? "opacity-0" : ""}`}
+                          wrapClassName={`transition-all duration-300 bg-orange-50 border border-red-500 border-solid h-[30px] rounded-[3px] pl-2 ml-[22px] w-[68px] ${number["set1"] === 0 ? "opacity-0" : ""}`}
+                          value={number["set1"]}
+                          type="number"
+                          onChange={(value) => handleInputNumber(value, "set1")}
+                          onBlur={() => handleInputBlur("set1")}
+                        />
+                      </div>
+                      <div className="flex flex-row items-center justify-start">
+                        <Checkbox
+                          {...label}
+                          checked={number["set2"] !== 0}
+                          color="success"
+                          onChange={() => handleCheckBoxClick("set2")}
+                        />
+                        <Text className="font-sfmono text-red-500 text-left">Set 2 cakes</Text>
+                        <Input
+                          className={`transition-all duration-300 leading-[normal] text-center text-lg text-red-500 h-full w-full ${number["set2"] === 0 ? "opacity-0" : ""}`}
+                          wrapClassName={`transition-all duration-300 bg-orange-50 border border-red-500 border-solid h-[30px] rounded-[3px] pl-2 ml-3 w-[68px] ${number["set2"] === 0 ? "opacity-0" : ""}`}
+                          value={number["set2"]}
+                          type="number"
+                          onChange={(value) => handleInputNumber(value, "set2")}
+                          onBlur={() => handleInputBlur("set2")}
+                        />
+                      </div>
+                      <div className="flex flex-row items-center justify-start">
+                        <Checkbox
+                          {...label}
+                          checked={number["set4"] !== 0}
+                          color="success"
+                          onChange={() => handleCheckBoxClick("set4")}
+                        />
+                        <Text className="font-sfmono text-red-500 text-left">Set 4 cakes</Text>
+                        <Input
+                          className={`transition-all duration-300 leading-[normal] text-center text-lg text-red-500 h-full w-full ${number["set4"] === 0 ? "opacity-0" : ""}`}
+                          wrapClassName={`transition-all duration-300 bg-orange-50 border border-red-500 border-solid h-[30px] rounded-[3px] pl-2 ml-3 w-[68px] ${number["set4"] === 0 ? "opacity-0" : ""}`}
+                          value={number["set4"]}
+                          type="number"
+                          onChange={(value) => handleInputNumber(value, "set4")}
+                          onBlur={() => handleInputBlur("set4")}
+                        />
+                      </div>
+                      <div className="flex flex-row items-center justify-start">
+                        <Checkbox
+                          {...label}
+                          checked={number["set6"] !== 0}
+                          color="success"
+                          onChange={() => handleCheckBoxClick("set6")}
+                        />
+                        <Text className="font-sfmono text-red-500 text-left">Set 6 cakes</Text>
+                        <Input
+                          className={`transition-all duration-300 leading-[normal] text-center text-lg text-red-500 h-full w-full ${number["set6"] === 0 ? "opacity-0" : ""}`}
+                          wrapClassName={`transition-all duration-300 bg-orange-50 border border-red-500 border-solid h-[30px] rounded-[3px] pl-2 ml-3 w-[68px] ${number["set6"] === 0 ? "opacity-0" : ""}`}
+                          value={number["set6"]}
+                          type="number"
+                          onChange={(value) => handleInputNumber(value, "set6")}
+                          onBlur={() => handleInputBlur("set6")}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <Text className="text-lg text-red-500 text-right">{number.set1 + number.set2 * 2 + number.set4 * 4 + number.set6 * 6} x {orderDetail.cake.price.toLocaleString("vi-VN")} VNĐ</Text>
+              </div>
+              <Line className="bg-red-500 h-px w-full" />
+            </div>
+            <div className="flex flex-row gap-5 items-center justify-center w-[52%] md:w-full">
+              <Button
+                className={`bg-orange-50 hover:bg-indigo-900 border border-indigo-900 hover:border-teal-100 border-solid leading-[normal] min-w-[193px] py-3.5 rounded-[5px] text-center text-indigo-900 hover:text-orange-50 text-lg ${number["set1"] === 0 && number["set2"] === 0 && number["set4"] === 0 && number["set6"] === 0 ? "cursor-not-allowed" : ""}`}
+                disabled={number["set1"] === 0 && number["set2"] === 0 && number["set4"] === 0 && number["set6"] === 0}
+                onClick={() => handleUpdateCart()}
+              >
+                save
+              </Button>
+              <Text className="font-sfmono text-red-500 text-left">or</Text>
+              <Button
+                className="bg-orange-50 hover:bg-red-500 border border-red-500 hover:border-teal-100 border-solid leading-[normal] min-w-[193px] py-3.5 rounded-[5px] text-center text-red-500 hover:text-orange-50 text-lg"
+                onClick={() => handleCloseEditPopup()}
+              >
+                close
+              </Button>
+            </div>
+          </div>
+        </div>
+      </Backdrop>
+    );
+  }
+
   const cakes = () => {
     const element = [];
 
@@ -171,6 +375,7 @@ const Navbar = (props) => {
                   className="cursor-pointer"
                   src={hoverEdit === index ? "images/icon_edit_hover.svg" : "images/icon_edit.svg"}
                   alt="edit"
+                  onClick={() => handleOpenEditPopup(orderDetail)}
                   onMouseEnter={() => handleIconEditEnter(index)}
                   onMouseLeave={() => handleIconEditLeave()}
                 />
@@ -187,10 +392,6 @@ const Navbar = (props) => {
     }
 
     return element;
-  }
-
-  const editCake = () => {
-      
   }
 
   const handleLogout = async () => {
@@ -323,6 +524,7 @@ const Navbar = (props) => {
         </div>
       </div>
       <Line className="absolute bg-red-500 h-px inset-x-[0] mx-auto bottom-[0] w-full" />
+      {orderDetail && number && editCake()}
     </div>
   );
 };
